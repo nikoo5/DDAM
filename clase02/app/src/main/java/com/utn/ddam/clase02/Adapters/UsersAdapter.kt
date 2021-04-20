a5228383cd573f7a80007d9327a35b397bac5975
Nicolas Filippi Farmar <nico.filippi@hotmail.com>
2021-04-20 19:12:46 -0300

    Clase 2 update

---
+++ clase02/app/src/main/java/com/utn/ddam/clase02/Adapters/UsersAdapter.kt
@@ -0,0 +1,31 @@
+package com.utn.ddam.clase02.Adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.utn.ddam.clase02.Entities.User
+import com.utn.ddam.clase02.R
+import kotlinx.android.synthetic.main.item_user.view.*
+
+class UsersAdapter(val users : List<User>) : RecyclerView.Adapter<UsersAdapter.UserHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return UserHolder(layoutInflater.inflate(R.layout.item_user, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: UserHolder, position: Int) {
+        holder.render(users[position])
+    }
+
+    override fun getItemCount(): Int = users.size
+
+    class UserHolder(val view : View) : RecyclerView.ViewHolder(view) {
+        fun render(user: User) {
+            view.tvUsername.text = user.username;
+            view.tvName.text = user.name + " " + user.lastname;
+            view.tvEmail.text = user.email;
+        }
+    }
+}
--- clase02/app/src/main/java/com/utn/ddam/clase02/Fragments/LoginFragment.kt
+++ clase02/app/src/main/java/com/utn/ddam/clase02/Fragments/LoginFragment.kt
@@ -1,67 +1,43 @@
 package com.utn.ddam.clase02.Fragments
 
-import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.utn.ddam.clase02.Entities.User
 import com.utn.ddam.clase02.R
-import kotlinx.android.synthetic.main.fragment_login.*
 
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Login.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Login : Fragment() {
-    lateinit var v : View;
+class LoginFragment : Fragment() {
+    lateinit var v : View
     lateinit var loginLayout : ConstraintLayout
     lateinit var btnLogin: Button
-    lateinit var txtUsername: EditText
-    lateinit var txtPassword: EditText
+    lateinit var txtLoginUsername: EditText
+    lateinit var txtLoginPassword: EditText
 
     var lstUsers : MutableList<User> = mutableListOf();
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         v =  inflater.inflate(R.layout.fragment_login, container, false)
 
         loginLayout = v.findViewById(R.id.loginLayout)
         btnLogin = v.findViewById(R.id.btnLogin);
-        txtUsername = v.findViewById(R.id.txtUsername);
-        txtPassword = v.findViewById(R.id.txtPassword);
+        txtLoginUsername = v.findViewById(R.id.txtLoginUsername);
+        txtLoginPassword = v.findViewById(R.id.txtLoginPassword);
 
-        lstUsers.add(
-            User(
-                "nfilippi",
-                "1234",
-                "nico.filippi@hotmail.com",
-                "Nicolás",
-                "Filippi Farmar"
-            )
-        )
-        lstUsers.add(
-            User(
-                "abreitman",
-                "1122",
-                "alejandro.breitman@hotmail.com",
-                "Alejandro",
-                "Breitman"
-            )
-        )
+        lstUsers.add(User("nfilippi", "1234", "nfilippi@hotmail.com", "Nicolás", "Filippi Farmar"))
+        lstUsers.add(User("abreitman", "1122", "abreitman@hotmail.com", "Alejandro", "Breitman"))
         lstUsers.add(User("jarrieta", "4321", "jarrieta@hotmail.com", "Javier", "Arrieta"))
         lstUsers.add(User("jperez", "7890", "jperez@hotmail.com", "Josefina", "Pérez"))
 
@@ -72,8 +48,8 @@ class Login : Fragment() {
         super.onStart()
 
         btnLogin.setOnClickListener {
-            val username = txtUsername.text.trim().toString()
-            val password = txtPassword.text.trim().toString()
+            val username = txtLoginUsername.text.trim().toString()
+            val password = txtLoginPassword.text.trim().toString()
 
             if(username.isNotEmpty() && password.isNotEmpty()) {
                 var user : User? = lstUsers.find { (username.indexOf('@') >= 0 && it.email == username) || (username.indexOf(
@@ -82,7 +58,7 @@ class Login : Fragment() {
 
                 if(user != null) {
                     if(user.password == password) {
-                        val action = LoginDirections.loginToHome(user.Greetings());
+                        val action = LoginFragmentDirections.loginToHome(user, lstUsers.toTypedArray())
                         v.findNavController().navigate(action);
                     } else {
                         Snackbar.make(loginLayout, "Contraseña Incorrecta", Snackbar.LENGTH_SHORT).show();
